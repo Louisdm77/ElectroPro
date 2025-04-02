4973380492e321a6bb4e0219343b2388382154bd
Akwaji Dan Uke <danakwaji3@gmail.com>
2025-04-02 16:20:24 +0100

    added scroll to the top solution on pathchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -7,10 +7,21 @@ import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top on route change
+  }, [pathname]);
+
+  return null; // This component doesn’t render anything
+};
+
 const App = () => {
   return (
     <div className="bg-gray-50 min-h-screen font-sans">
       <Navbar />
+      <ScrollToTop /> {/* Add scroll restoration */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
